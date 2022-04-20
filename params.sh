fdd3c15dc8bae89ebd57421e53203251e838f66e
TeamTestEngineers <numerouno51@gmail.com>
2022-04-20 19:17:32 +0000

    debugmode

---
+++ params.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo $0 # this line will display the file name
+echo $? # this line will give the status code. 
+echo $1 # this will give the 1st parameter 
+echo $2 # this will give the 2nd parameter
+echo $$
